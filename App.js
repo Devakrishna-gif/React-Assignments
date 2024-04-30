1d767423b0f3ffe8757ad4c36028d58b67735406
Devakrishna Ramineni Sai Vijay <RS00847863@TechMahindra.com>
2024-04-30 15:27:56 +0530

    Header-code

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import flipkartlogo from './images/flipkart-logo.svg';
+import profileicon from './images/profile-icon.svg';
+import './App.css'
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <div className='container'>
+            <img src={flipkartlogo} />
+            <input className='searchBar' type='search' placeholder='Search for Products, Brands and More' />
+            <img src={profileicon} />
+
+        </div>
     )
 }
 
